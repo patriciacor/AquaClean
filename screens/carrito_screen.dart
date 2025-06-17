7eccc13737b26e1a7c526e0b81462aad322273b6
Patt Cor <jaimezpatricia@gmail.com>
2025-06-17 15:51:07 -0300

    Update carrito_screen.dart

--- screens/carrito_screen.dart
+++ screens/carrito_screen.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CarritoScreen extends StatelessWidget {
+  const CarritoScreen({Key? key}) : super(key: key);
+
+  void _goBack(BuildContext context) {
+    Navigator.pop(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: agregar lista de productos en carrito y total a pagar
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Carrito'),
+        backgroundColor: const Color(0xFF0077B6),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => _goBack(context),
+          child: const Text('Volver a Tienda'),
+        ),
+      ),
+    );
+  }
+}
 
