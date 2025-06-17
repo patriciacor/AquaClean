3af75dd37233d707baf36f84e4714b9079b951ea
Patt Cor <jaimezpatricia@gmail.com>
2025-06-17 15:51:42 -0300

    Update contacto_tecnico_screen.dart

--- screens/contacto_tecnico_screen.dart
+++ screens/contacto_tecnico_screen.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ContactoTecnicoScreen extends StatelessWidget {
+  const ContactoTecnicoScreen({Key? key}) : super(key: key);
+
+  void _goBack(BuildContext context) {
+    Navigator.pop(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: agregar formulario con calendario y mapa de ubicación
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contactar Técnico'),
+        backgroundColor: const Color(0xFF0077B6),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => _goBack(context),
+          child: const Text('Volver'),
+        ),
+      ),
+    );
+  }
+}
 
