841c9dfc2c7ce4ce8ed8de9ca0e43ed9878fbbf0
Patt Cor <jaimezpatricia@gmail.com>
2025-06-17 16:19:58 -0300

    Create producto.dart

---
+++ models/producto.dart
@@ -0,0 +1,15 @@
+class Producto {
+  final String id;
+  final String nombre;
+  final String descripcion;
+  final double precio;
+  final String imagen;
+
+  Producto({
+    required this.id,
+    required this.nombre,
+    required this.descripcion,
+    required this.precio,
+    required this.imagen,
+  });
+}
